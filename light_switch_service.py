47e8fde27760237af67a23636efa19a068f18af7
porzech <pworzechowski@gmail.com>
2014-09-05 12:32:07 +0200

    meged all code into a single file

--- light_switch_service.py
+++ light_switch_service.py
@@ -17,13 +17,36 @@ import getopt
 from mewa.client import Connection
 import time
 import threading
-from globalids import *
 
 try:
     import RPi.GPIO as GPIO
     GPIO.setmode(GPIO.BOARD)
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+'''Discovery service'''
+URI_DISCOVERY             = "org.fi24.discovery"
+URI_DISCOVERY_GETSERVICES = "org.fi24.discovery.GetServices"
+URI_DISCOVERY_SERVICELIST = "org.fi24.discovery.ServiceList"
+
+
+'''Switch service'''
+URI_SWITCH                = "org.fi24.switch"
+URI_SWITCH_GETSTATE       = "org.fi24.switch.GetState"
+URI_SWITCH_CURSTATE       = "org.fi24.switch.CurrentState"
+URI_SWITCH_EV_SWITCHON       = "org.fi24.switch.SwitchedOn"
+URI_SWITCH_EV_SWITCHOFF       = "org.fi24.switch.SwitchedOff"
+
+'''Light service'''
+URI_LIGHT                 ="org.fi24.light"
+URI_LIGHT_CMD_ON          ="org.fi24.light.TurnOn"
+URI_LIGHT_CMD_OFF          ="org.fi24.light.TurnOff"
+URI_LIGHT_CMD_TOGGLE       ="org.fi24.light.Toggle"
+URI_LIGHT_EVT_TURNEDON       ="org.fi24.light.TurnedOn"
+URI_LIGHT_EVT_TURNEDOFF       ="org.fi24.light.TurnedOff"
+
+
 
 LIGHT_DEVICE_NAME_PREFIX = "RasPi_Light-"
 '''Name, which the device uses to connect to the channel'''
